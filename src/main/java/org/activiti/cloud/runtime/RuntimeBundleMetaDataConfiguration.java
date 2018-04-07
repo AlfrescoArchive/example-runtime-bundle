476d5d580aef210a60d052700e1ebb406677aa25
Salaboy <salaboy@gmail.com>
2018-04-07 10:47:23 +0100

    adding metadata auto config

--- src/main/java/org/activiti/cloud/runtime/RuntimeBundleMetaDataConfiguration.java
+++ src/main/java/org/activiti/cloud/runtime/RuntimeBundleMetaDataConfiguration.java
@@ -2,11 +2,12 @@ package org.activiti.cloud.runtime;
 
 import java.util.HashMap;
 import java.util.Map;
+import javax.annotation.PostConstruct;
 
 import com.netflix.appinfo.ApplicationInfoManager;
 import org.springframework.beans.factory.BeanClassLoaderAware;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,12 +15,24 @@ import org.springframework.context.annotation.Configuration;
 @ConditionalOnClass(ApplicationInfoManager.class)
 public class RuntimeBundleMetaDataConfiguration implements BeanClassLoaderAware {
 
-    // This code needs to live here until we find the right abstraction for registering/updating Service Metadata
+    @Value("${activiti.cloud.application.name:}")
+    private String applicationName;
+
     @Autowired
-    public RuntimeBundleMetaDataConfiguration(ApplicationInfoManager appInfoManager) {
+    private ApplicationInfoManager appInfoManager;
+    // This code needs to live here until we find the right abstraction for registering/updating Service Metadata
+
+    public RuntimeBundleMetaDataConfiguration() {
+
+    }
+
+    @PostConstruct
+    public void init() {
         Map<String, String> metadata = new HashMap<>();
         metadata.put("activiti-cloud-service-type",
                      "runtime-bundle");
+        metadata.put("activiti-cloud-application-name",
+                     applicationName);
         appInfoManager.registerAppMetadata(metadata);
     }
 
