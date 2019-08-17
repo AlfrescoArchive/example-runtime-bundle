bb9768db08f2a31dd73f0f439a5d87f558823924
Igor Dianov <igor@dianov.com>
2019-08-17 00:37:37 -0700

    fix: isolate process-destinatino-location property test

--- src/test/java/org/activiti/cloud/runtime/RuntimeBundleApplicationIT.java
+++ src/test/java/org/activiti/cloud/runtime/RuntimeBundleApplicationIT.java
@@ -1,15 +1,14 @@
 package org.activiti.cloud.runtime;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = RuntimeBundleApplication.class)
+@SpringBootTest(classes = RuntimeBundleApplication.class,
+                properties = "spring.activiti.process-definition-location-prefix=file:/processes/")
 @DirtiesContext
 public class RuntimeBundleApplicationIT {
 
